a603d4326b7b189e9a39888b0a883450021da321
Gordon Hall <gordon@gordonwritescode.com>
2016-01-08 14:39:36 -0500

    add placeholder docs, add initial storage models, add storage interface for mongodb

---
+++ doc/server.md
@@ -0,0 +1,2 @@
+Class: Server(options)
+======================
---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * @module metadisk
+ */
+
+'use strict';
+
+module.exports = {
+  Engine: require('./lib/engine'),
+  Config: require('./lib/config'),
+  Storage: require('./lib/storage'),
+  Server: require('./lib/server'),
+  Network: require('./lib/network')
+};
