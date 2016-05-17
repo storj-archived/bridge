73306793de951d5fbe90867eaa29c0da148f6beb
MrFancyMonocle <ryanforan@gmail.com>
2016-05-17 11:38:54 -0400

    fixed run bugs

--- bin/audit-service.js
+++ bin/audit-service.js
@@ -2,34 +2,42 @@
 
 'use strict';
 
-const Config   = require('../config').audits;
+const Config   = require('../lib/config')(process.env.NODE_ENV || 'devel').audits;
+const log      = require('../lib/logger');
 const fork     = require('child_process').fork;
 const numCores = require('os').cpus().length;
 const argv     = require('minimist')(process.argv.slice(2));
-const workers  = argv.workers;
-const offset   = argv.offset;
-const Auditor  = '../lib/audit';
+const Auditor  = './lib/audit';
 
 const AuditService = function() {
-  var workers = [];
-  var options = [Config.limit, Config.frequency];
-  this.offset = offset || 0;
-  this.workers = workers || numCores - this.offset;
+  var offset     = argv.offset || 0;
+  var numWorkers = argv.workers || numCores - offset;
+  var workers    = {};
+  var options    = [Config.limit, Config.frequency];
 
-  console.log('Master forking ' + numCores + ' workers...');
-  for(let i = 0; i < this.workers; i++) {
-    workers[i] = fork(Auditor, options);
+  log.info('Master forking ' + numWorkers + ' workers...');
 
-    workers[i].on('exit', function(code, signal) {
-      console.log('Worker: ' + workers[i].pid + ' died with code: ' + code + ', and signal: ' + signal);
-      console.log('restarting worker...');
-      workers[i].fork(Auditor, options);
+  for(let i = 0; i < numWorkers; i++) {
+    workers = addNewWorkerToQueue(workers);
+  }
+
+  function addNewWorkerToQueue(queue) {
+    let newWorker = fork(Auditor, options);
+    log.info('starting worker: ' + newWorker.pid);
+    queue[newWorker.pid] = newWorker;
+
+    queue[newWorker.pid].on('exit', function(code, signal) {
+      delete queue[this.pid];
+      log.info('Worker pid:' + this.pid + ' exited with code: ' + code + ', signal: ' + signal);
+      addNewWorkerToQueue(queue);
     });
 
-    workers[i].on('error', function(err) {
-      console.log(err);
+    queue[newWorker.pid].on('error', function(err) {
+      log.error(err);
     });
+
+    return queue;
   }
 }
 
-module.exports = AuditService;
+module.exports = AuditService();
