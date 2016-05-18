109937a035c62c4da520f1a19d45c46a53b5ce62
MrFancyMonocle <ryanforan@gmail.com>
2016-05-18 13:41:05 -0400

    adding config-based startup, removing arguments

--- bin/audit-service.js
+++ bin/audit-service.js
@@ -5,30 +5,35 @@
 const Config   = require('../lib/config')(process.env.NODE_ENV || 'devel').audits;
 const log      = require('../lib/logger');
 const fork     = require('child_process').fork;
-const numCores = require('os').cpus().length;
-const argv     = require('minimist')(process.argv.slice(2));
+//const numCores = require('os').cpus().length;
+//const argv     = require('minimist')(process.argv.slice(2));
 const Auditor  = './lib/audit';
 
 const AuditService = function() {
-  var offset     = argv.offset || 0;
-  var numWorkers = argv.workers || numCores - offset;
-  var workers    = {};
-  var options    = [Config.limit, Config.frequency];
+  console.log(Config)
+  const numWorkers = Config.length;
+  var workers      = {};
 
-  log.info('Master forking ' + numWorkers + ' workers...');
+  log.info('master audit service forking ' + numWorkers + ' workers...');
 
   for(let i = 0; i < numWorkers; i++) {
-    workers = addNewWorkerToQueue(workers);
+    let options = [
+      Config[i].limit,
+      Config[i].frequency,
+      JSON.stringify(Config[i].redis),
+      JSON.stringify(Config[i].network)
+    ];
+    workers = addNewWorkerToQueue(workers, Auditor, options);
   }
 
-  function addNewWorkerToQueue(queue) {
-    let newWorker = fork(Auditor, options);
-    log.info('starting worker: ' + newWorker.pid);
+  function addNewWorkerToQueue(queue, module, opts) {
+    var newWorker = fork(module, opts);
+    log.info('starting worker pid: ' + newWorker.pid);
     queue[newWorker.pid] = newWorker;
 
     queue[newWorker.pid].on('exit', function(code, signal) {
       delete queue[this.pid];
-      log.info('Worker pid:' + this.pid + ' exited with code: ' + code + ', signal: ' + signal);
+      log.info('worker pid:' + this.pid + ' exited with code: ' + code + ', signal: ' + signal);
       addNewWorkerToQueue(queue);
     });
 
