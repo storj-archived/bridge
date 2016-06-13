e75606c867170f48da0d4bc3f142b6c3bcc812b6
MrFancyMonocle <ryanforan@gmail.com>
2016-06-13 17:55:04 -0400

    adding blocking queue pops to avoid polling, reworking workers, updated config

--- bin/audit-service.js
+++ bin/audit-service.js
@@ -2,46 +2,56 @@
 
 'use strict';
 
-const Config   = require('../lib/config')(process.env.NODE_ENV || 'devel').audits;
-const log      = require('../lib/logger');
-const fork     = require('child_process').fork;
+const Config = require('../lib/config')(process.env.NODE_ENV || 'devel').audits;
+const log = require('../lib/logger');
+const fork = require('child_process').fork;
 //const numCores = require('os').cpus().length;
 //const argv     = require('minimist')(process.argv.slice(2));
-const Auditor  = './lib/audit';
+const INTERVAL = 10000;
+const Auditor = './lib/audit';
+const AuditQueue = require('../lib/audit/queue.js');
 
-const AuditService = function() {
-  const numWorkers = Config.length;
-  this.workers     = {};
+const AuditService = function(options) {
+  var self = this;
+  this._options = options;
+  this._workers = {};
+  log.info('master audit service forking ' + this._options.workers.length + ' workers...');
 
-  log.info('master audit service forking ' + numWorkers + ' workers...');
+  this._masterPollQueue = new AuditQueue(this._options.redis);
+  this._interval = setInterval(this.pollBacklog, INTERVAL);
 
-  for(let i = 0; i < numWorkers; i++) {
-    let options = [
-      Config[i].limit,
-      Config[i].frequency,
-      JSON.stringify(Config[i].redis),
-      JSON.stringify(Config[i].network)
-    ];
-    this.workers = addNewWorkerToQueue(this.workers, Auditor, options);
-  }
-
-  function addNewWorkerToQueue(queue, module, opts) {
-    var newWorker = fork(module, opts);
-    log.info('starting worker pid: ' + newWorker.pid);
-    queue[newWorker.pid] = newWorker;
-
-    queue[newWorker.pid].on('exit', function(code, signal) {
-      delete queue[this.pid];
-      log.info('worker pid:' + this.pid + ' exited with code: ' + code + ', signal: ' + signal);
-      addNewWorkerToQueue(queue, Auditor, opts);
-    });
-
-    queue[newWorker.pid].on('error', function(err) {
-      log.error(err);
-    });
-
-    return queue;
-  }
+  this._options.workers.forEach(function(workerConfig, ind) {
+    self.addNewWorkerToQueue(workerConfig);
+  });
 };
 
-module.exports = new AuditService();
+AuditService.prototype.addNewWorkerToQueue = function(workerConfig) {
+  var self = this;
+  var opts = Object.assign(workerConfig, {redis: this._options.redis});
+  var newWorker = fork(Auditor, [JSON.stringify(opts)]);
+
+  log.info('starting worker pid: ' + newWorker.pid);
+  this._workers[newWorker.pid] = newWorker;
+
+  this._workers[newWorker.pid].on('exit', function(code, signal) {
+    delete self._workers[this.pid];
+    log.info('worker pid:' + this.pid + ' exited with code: ' + code + ', signal: ' + signal);
+    self.addNewWorkerToQueue(opts);
+  });
+
+  this._workers[newWorker.pid].on('error', function(err) {
+    log.error(err);
+  });
+};
+
+AuditService.prototype.pollBacklog = function() {
+  var currTime = Math.floor(new Date() / 1000);
+  this._masterPollQueue.populateReadyQueue(
+    0,
+    currTime,
+    function(err, hasAudits) {
+      if(err) log.error(err);
+  });
+}
+
+new AuditService(Config);
