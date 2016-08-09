263edd3ea44959674dbcf42c4a6e9c445604e251
Dylan Lott <lott.dylan@gmail.com>
2016-08-09 16:35:19 -0400

    WIP adding ruby mongoid database generators

--- script/generate/factory.rb
+++ script/generate/factory.rb
@@ -1,1 +1,27 @@
-puts 'hello from ruby'
+require_relative '../../gems/ruby/2.2.0/gems/factory_girl'
+include FactoryGirl
+require 'mongoid'
+require 'database_cleaner'
+
+Mongoid.load!("#{__dir}/mongoid.yml", :development)
+
+class User
+  include Mongoid::Document
+end
+
+define do
+  sequence :email do |n|
+    "user#{n}@example.com"
+  end
+  # factory :credit do
+  #   amount 10000
+  #   user
+  # end
+
+  factory :user do
+    _id "test@example.com"
+    # credits
+  end
+end
+
+create :user
