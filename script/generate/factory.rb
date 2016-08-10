02342f1eb57fc7e77901e1db3111ba245da6e188
Dylan Lott <lott.dylan@gmail.com>
2016-08-10 17:00:51 -0400

    got ruby mock data generation script running, started working on graphql endpoints and schemas

--- script/generate/factory.rb
+++ script/generate/factory.rb
@@ -7,16 +7,34 @@ include FactoryGirl
 
 Mongoid.load!("#{__dir__}/mongoid.yml", :development)
 
+DatabaseCleaner.strategy = :truncation
+DatabaseCleaner.clean
+
 class User
   include Mongoid::Document
-  embeds_many :credits
+  has_many :credits
+  has_many :debits
 end
 
 class Credit
   include Mongoid::Document
   field :amount, type: Integer
-  embeds_one :user
+  belongs_to :user
 end
+
+class Debit
+  include Mongoid::Document
+  field :amount, type: Integer
+  belongs_to :user
+end
+
+# user = User.create _id: "lott.dylan@gmail.com"
+#
+# 5.times do |i|
+#   credit = Credit.create amount: 10000 + i, user: user
+# end
+#
+# puts(user.credits)
 
 FactoryGirl.define do
   sequence :_id do |n|
@@ -27,11 +45,18 @@ FactoryGirl.define do
     user
   end
 
+  factory :debit do
+    amount 10000
+    user
+  end
+
   factory :user do
     _id
-    transient {credits_count 5}
+    transient {credits_count 4}
+    transient {debits_count 5}
     after(:create) do |user, evaluator|
-      create_list(:credit, evaluator.credits_count, user: user)
+      credits = create_list(:credit, evaluator.credits_count, user: user)
+      debits = create_list(:debit, evaluator.debits_count, user: user)
     end
   end
 end
