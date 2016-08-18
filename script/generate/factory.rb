6b65305f73dff52a8baa9cf3ebab95a011563584
Bryan White <bryanchriswhite@gmail.com>
2016-08-18 19:13:12 +0200

    update generator

--- script/generate/factory.rb
+++ script/generate/factory.rb
@@ -31,6 +31,7 @@ class Credit
 
   field :amount, type: Integer
   field :created, type: DateTime
+  field :type, type: String
 end
 
 class Debit
@@ -39,6 +40,7 @@ class Debit
 
   field :amount, type: Integer
   field :created, type: DateTime
+  field :type, type: String
 end
 
 FactoryGirl.define do
@@ -50,16 +52,33 @@ FactoryGirl.define do
     random_date
   end
 
+  sequence :audit? do
+    Random.rand(0..1) < 1
+  end
+
+
+  sequence :automatic? do
+    Random.rand(1..5) < 5 #1:5 manual:automatic
+  end
+
   factory :credit do
-    amount 10000
+    transient { automatic? } #1:1 audit:transfer
+    amount { Random.rand(200..1500) }
+    type { automatic? ? 'automatic' : 'manual' }
     created
     user
   end
 
   factory :debit do
-    amount 10000
+    transient { audit? }
+    amount { audit? ? Random.rand(1..500) : Random.rand(10..1000) }
+    type { audit? ? 'audit' : 'transfer' }
     created
     user
+
+    after() do |debit, evaluator|
+
+    end
   end
 
   factory :user do
@@ -67,8 +86,8 @@ FactoryGirl.define do
     hashpass '3f432dff8834d517de9ed5428bad0df117b30894bff4eed4d2d515e4bc48bc7f' #badpassword
     activated true
     created DateTime.now - 35 #35 days ago
-    transient {credits_count 4}
-    transient {debits_count 5}
+    transient {credits_count 8}
+    transient {debits_count 25}
     after(:create) do |user, evaluator|
       credits = create_list(:credit, evaluator.credits_count, user: user)
       debits = create_list(:debit, evaluator.debits_count, user: user)
