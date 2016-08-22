a44a4ef094e0f2d23d98b7dda09d3e5b4704a181
Bryan White <bryanchriswhite@gmail.com>
2016-08-22 17:28:49 +0200

    Update generator:
    + distribute credits and debits across months more realistically
    + end with debits and never a credit (so there's a balance/current usage)
    + set credit to debit ratio at 1:5
    + randomize created down to the minute instead of just hour
    + refactor/cleanup

--- script/generate/factory.rb
+++ script/generate/factory.rb
@@ -11,8 +11,18 @@ Mongoid.load!("#{__dir__}/mongoid.yml", :development)
 DatabaseCleaner.strategy = :truncation
 DatabaseCleaner.clean
 
-def random_date
-  DateTime.now - (Random.rand(1..720)/24.0) #sometime within the last 30 days
+DEBITS_PER_CREDIT = 5
+
+def random_date_between_months_ago(min_months_ago, max_months_ago)
+  random_days = Random.rand(
+    (min_months_ago * 30 * 24 * 60)..(max_months_ago * 30 * 24 * 60)
+  ) / 60.0 / 24.0
+  DateTime.now - random_days
+end
+
+def dates_per_month(count, per_month)
+  months_ago = count/per_month
+  random_date_between_months_ago(months_ago, (months_ago + 1))
 end
 
 class User
@@ -48,12 +58,15 @@ FactoryGirl.define do
     "user#{n}@example.com"
   end
 
-  sequence :created do
-    random_date
+  sequence :credit_index do |n|
+    n
+  end
+  sequence :debit_index do |n|
+    n
   end
 
   sequence :audit? do
-    Random.rand(0..1) < 1
+    Random.rand(0..1) < 1 #1:1 audit:transfer
   end
 
 
@@ -62,10 +75,11 @@ FactoryGirl.define do
   end
 
   factory :credit do
-    transient { automatic? } #1:1 audit:transfer
+    transient { automatic? }
     amount { Random.rand(200..1500) }
     type { automatic? ? 'automatic' : 'manual' }
-    created
+    transient { credit_index }
+    created { dates_per_month(credit_index, 1) }
     user
   end
 
@@ -73,21 +87,18 @@ FactoryGirl.define do
     transient { audit? }
     amount { audit? ? Random.rand(1..500) : Random.rand(10..1000) }
     type { audit? ? 'audit' : 'transfer' }
-    created
+    transient { debit_index }
+    created { dates_per_month(debit_index, 5) }
     user
-
-    after() do |debit, evaluator|
-
-    end
   end
 
   factory :user do
     _id
     hashpass '3f432dff8834d517de9ed5428bad0df117b30894bff4eed4d2d515e4bc48bc7f' #badpassword
     activated true
-    created DateTime.now - 35 #35 days ago
-    transient {credits_count 8}
-    transient {debits_count 25}
+    created DateTime.now - 365 #1 year ago
+    transient { credits_count 5 }
+    transient { debits_count (5 * (DEBITS_PER_CREDIT + 1)) }
     after(:create) do |user, evaluator|
       credits = create_list(:credit, evaluator.credits_count, user: user)
       debits = create_list(:debit, evaluator.debits_count, user: user)
