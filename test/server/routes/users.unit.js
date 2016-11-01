45f5ca9c91e2d38df5ab816eb986cb7781cf1351
Gordon Hall <gordon@gordonwritescode.com>
2016-11-01 15:42:50 -0400

    users router coverage + bugfix

--- test/server/routes/users.unit.js
+++ test/server/routes/users.unit.js
@@ -1,55 +1,432 @@
-/* jshint unused:false */ // TODO: REMOVE ME
-
 'use strict';
 
+const httpMocks = require('node-mocks-http');
+const sinon = require('sinon');
+const storj = require('storj-lib');
 const expect = require('chai').expect;
+const EventEmitter = require('events').EventEmitter;
 const UsersRouter = require('../../../lib/server/routes/users');
 
 describe('UsersRouter', function() {
 
+  var usersRouter = new UsersRouter(
+    require('../../_fixtures/router-opts')
+  );
+  var someUser = new usersRouter.storage.models.User({
+    _id: 'gordon@storj.io',
+    hashpass: storj.utils.sha256('password')
+  });
+
   describe('#_dispatchActivationEmail', function() {
 
-    it.skip('should callback error if mailer fails');
+    it('should callback error if mailer fails', function(done) {
+      var _dispatch = sinon.stub(
+        usersRouter.mailer,
+        'dispatch'
+      ).callsArgWith(3, new Error('Failed to send mail'));
+      usersRouter._dispatchActivationEmail(
+        new usersRouter.storage.models.User({
+          _id: 'gordon@storj.io'
+        }),
+        true,
+        function(err) {
+          _dispatch.restore();
+          expect(err.message).to.equal('Failed to send mail');
+          done();
+        }
+      );
+    });
 
-    it.skip('should callback null if mailer succeeds');
+    it('should callback null if mailer succeeds', function(done) {
+      var _dispatch = sinon.stub(
+        usersRouter.mailer,
+        'dispatch'
+      ).callsArgWith(3, null);
+      usersRouter._dispatchActivationEmail(
+        new usersRouter.storage.models.User({
+          _id: 'gordon@storj.io'
+        }),
+        true,
+        function(err) {
+          _dispatch.restore();
+          expect(err).to.equal(null);
+          done();
+        }
+      );
+    });
 
   });
 
   describe('#createUser', function() {
 
-    it.skip('should callback error if creation fails');
+    it('should callback error if creation fails', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/users',
+        body: {
+          email: 'gordon@storj.io',
+          password: storj.utils.sha256('password')
+        }
+      });
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _userCreate = sinon.stub(
+        usersRouter.storage.models.User,
+        'create'
+      ).callsArgWith(2, new Error('Panic!'));
+      usersRouter.createUser(request, response, function(err) {
+        _userCreate.restore();
+        expect(err.message).to.equal('Panic!');
+        done();
+      });
+    });
 
-    it.skip('should respond with user if creation succeeds');
+    it('should respond with user if creation succeeds', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/users',
+        body: {
+          email: 'gordon@storj.io',
+          password: storj.utils.sha256('password')
+        }
+      });
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _dispatchActivationEmail = sinon.stub(
+        usersRouter,
+        '_dispatchActivationEmail'
+      );
+      var _userCreate = sinon.stub(
+        usersRouter.storage.models.User,
+        'create'
+      ).callsArgWith(2, null, someUser);
+      response.on('end', function() {
+        _userCreate.restore();
+        _dispatchActivationEmail.restore();
+        expect(response._getData().email).to.equal('gordon@storj.io');
+        done();
+      });
+      usersRouter.createUser(request, response);
+    });
 
-    it.skip('should destroy user with bad request if pubkey fails');
+    it('should destroy user with bad request if pubkey fails', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/users',
+        body: {
+          email: 'gordon@storj.io',
+          password: storj.utils.sha256('password'),
+          pubkey: storj.KeyPair().getPublicKey()
+        }
+      });
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _dispatchActivationEmail = sinon.stub(
+        usersRouter,
+        '_dispatchActivationEmail'
+      );
+      var _userCreate = sinon.stub(
+        usersRouter.storage.models.User,
+        'create'
+      ).callsArgWith(2, null, someUser);
+      var _pubkeyCreate = sinon.stub(
+        usersRouter.storage.models.PublicKey,
+        'create'
+      ).callsArgWith(2, new Error('Failed to create pubkey'));
+      var _userRemove = sinon.stub(someUser, 'remove');
+      usersRouter.createUser(request, response, function(err) {
+        _userCreate.restore();
+        _dispatchActivationEmail.restore();
+        _pubkeyCreate.restore();
+        _userRemove.restore();
+        expect(_userRemove.called).to.equal(true);
+        expect(err.message).to.equal('Failed to create pubkey');
+        done();
+      });
+    });
 
-    it.skip('should respond with user if user and pubkey succeed');
+    it('should respond with user if user and pubkey succeed', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/users',
+        body: {
+          email: 'gordon@storj.io',
+          password: storj.utils.sha256('password'),
+          pubkey: storj.KeyPair().getPublicKey()
+        }
+      });
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _dispatchActivationEmail = sinon.stub(
+        usersRouter,
+        '_dispatchActivationEmail'
+      );
+      var _userCreate = sinon.stub(
+        usersRouter.storage.models.User,
+        'create'
+      ).callsArgWith(2, null, someUser);
+      var _pubkeyCreate = sinon.stub(
+        usersRouter.storage.models.PublicKey,
+        'create'
+      ).callsArgWith(2, null, { key: 'pubkey' });
+      response.on('end', function() {
+        _userCreate.restore();
+        _dispatchActivationEmail.restore();
+        _pubkeyCreate.restore();
+        var result = response._getData();
+        expect(result.email).to.equal('gordon@storj.io');
+        expect(result.pubkey).to.equal('pubkey');
+        done();
+      });
+      usersRouter.createUser(request, response);
+    });
 
   });
 
   describe('#confirmActivateUser', function() {
 
-    it.skip('should internal error if query fails');
+    it('should internal error if query fails', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/activations/token',
+        params: {
+          token: 'token'
+        }
+      });
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _userFindOne = sinon.stub(
+        usersRouter.storage.models.User,
+        'findOne'
+      ).callsArgWith(1, new Error('Panic!'));
+      usersRouter.confirmActivateUser(request, response, function(err) {
+        _userFindOne.restore();
+        expect(err.message).to.equal('Panic!');
+        done();
+      });
+    });
 
-    it.skip('should bad request error if bad token');
+    it('should bad request error if bad token', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/activations/token',
+        params: {
+          token: 'token'
+        }
+      });
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _userFindOne = sinon.stub(
+        usersRouter.storage.models.User,
+        'findOne'
+      ).callsArgWith(1, null, null);
+      usersRouter.confirmActivateUser(request, response, function(err) {
+        _userFindOne.restore();
+        expect(err.message).to.equal('Invalid activation token');
+        done();
+      });
+    });
 
-    it.skip('should internal if activate fails');
+    it('should internal if activate fails', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/activations/token',
+        params: {
+          token: 'token'
+        }
+      });
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _userFindOne = sinon.stub(
+        usersRouter.storage.models.User,
+        'findOne'
+      ).callsArgWith(1, null, someUser);
+      var _userActivate = sinon.stub(someUser, 'activate').callsArgWith(
+        0,
+        new Error('Failed to activate user')
+      );
+      usersRouter.confirmActivateUser(request, response, function(err) {
+        _userFindOne.restore();
+        _userActivate.restore();
+        expect(err.message).to.equal('Failed to activate user');
+        done();
+      });
+    });
 
-    it.skip('should redirect if success and redirect specified');
+    it('should redirect if success and redirect specified', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/activations/token',
+        params: {
+          token: 'token'
+        },
+        query: {
+          redirect: 'someredirecturl'
+        }
+      });
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _userFindOne = sinon.stub(
+        usersRouter.storage.models.User,
+        'findOne'
+      ).callsArgWith(1, null, someUser);
+      var _userActivate = sinon.stub(someUser, 'activate').callsArg(0);
+      response.redirect = function() {
+        _userFindOne.restore();
+        _userActivate.restore();
+        delete response.redirect;
+        done();
+      };
+      usersRouter.confirmActivateUser(request, response);
+    });
 
-    it.skip('should respond with user if success');
+    it('should respond with user if success', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/activations/token',
+        params: {
+          token: 'token'
+        }
+      });
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _userFindOne = sinon.stub(
+        usersRouter.storage.models.User,
+        'findOne'
+      ).callsArgWith(1, null, someUser);
+      var _userActivate = sinon.stub(someUser, 'activate').callsArg(0);
+      response.on('end', function() {
+        _userFindOne.restore();
+        _userActivate.restore();
+        expect(response._getData().email).to.equal('gordon@storj.io')
+        done();
+      });
+      usersRouter.confirmActivateUser(request, response);
+    });
 
   });
 
   describe('#reactivateUser', function() {
 
-    it.skip('should internal error if query fails');
+    it('should internal error if query fails', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/activations',
+        body: {
+          email: 'gordon@storj.io'
+        }
+      });
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _userFindOne = sinon.stub(
+        usersRouter.storage.models.User,
+        'findOne'
+      ).callsArgWith(1, new Error('Panic!'));
+      usersRouter.reactivateUser(request, response, function(err) {
+        _userFindOne.restore();
+        expect(err.message).to.equal('Panic!');
+        done();
+      });
+    });
 
-    it.skip('should not found error if no user found');
+    it('should not found error if no user found', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/activations',
+        body: {
+          email: 'gordon@storj.io'
+        }
+      });
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _userFindOne = sinon.stub(
+        usersRouter.storage.models.User,
+        'findOne'
+      ).callsArgWith(1, null, null);
+      usersRouter.reactivateUser(request, response, function(err) {
+        _userFindOne.restore();
+        expect(err.message).to.equal('User not found');
+        done();
+      });
+    });
 
-    it.skip('should bad request if user already activated');
+    it('should bad request if user already activated', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/activations',
+        body: {
+          email: 'gordon@storj.io'
+        }
+      });
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      someUser.activated = true;
+      var _userFindOne = sinon.stub(
+        usersRouter.storage.models.User,
+        'findOne'
+      ).callsArgWith(1, null, someUser);
+      usersRouter.reactivateUser(request, response, function(err) {
+        _userFindOne.restore();
+        expect(err.message).to.equal('User is already activated');
+        done();
+      });
+    });
 
-    it.skip('should send back user if activate success');
+    it('should send back user if activate success', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/activations',
+        body: {
+          email: 'gordon@storj.io'
+        }
+      });
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      someUser.activated = false;
+      var _userFindOne = sinon.stub(
+        usersRouter.storage.models.User,
+        'findOne'
+      ).callsArgWith(1, null, someUser);
+      var _dispatchActivationEmail = sinon.stub(
+        usersRouter,
+        '_dispatchActivationEmail'
+      );
+      response.on('end', function(err) {
+        _userFindOne.restore();
+        _dispatchActivationEmail.restore();
+        expect(_dispatchActivationEmail.called).to.equal(true);
+        var result = response._getData();
+        expect(result.email).to.equal('gordon@storj.io');
+        done();
+      });
+      usersRouter.reactivateUser(request, response);
+    });
 
   });
 
