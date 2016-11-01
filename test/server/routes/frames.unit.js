51e70f6662f6f479d69e56e99bd24635ee4f918c
Gordon Hall <gordon@gordonwritescode.com>
2016-11-01 12:57:25 -0400

    frame router coverage almost up to 100

--- test/server/routes/frames.unit.js
+++ test/server/routes/frames.unit.js
@@ -1,5 +1,3 @@
-/* jshint unused:false */ // TODO: REMOVE ME
-
 'use strict';
 
 const httpMocks = require('node-mocks-http');
@@ -21,19 +19,153 @@ describe('FramesRouter', function() {
 
   describe('#createFrame', function() {
 
-    it.skip('should return internal error if create fails');
+    it('should return internal error if create fails', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/frames'
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _frameCreate = sinon.stub(
+        framesRouter.storage.models.Frame,
+        'create'
+      ).callsArgWith(1, new Error('Panic!'));
+      framesRouter.createFrame(request, response, function(err) {
+        _frameCreate.restore();
+        expect(err.message).to.equal('Panic!');
+        done();
+      });
+    });
 
-    it.skip('should return the created frame');
+    it('should return the created frame', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/frames'
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var frame1 = new framesRouter.storage.models.Frame({
+        user: someUser._id
+      });
+      var _frameCreate = sinon.stub(
+        framesRouter.storage.models.Frame,
+        'create'
+      ).callsArgWith(1, null, frame1);
+      response.on('end', function() {
+        _frameCreate.restore();
+        expect(response._getData().user).to.equal(someUser._id);
+        done();
+      });
+      framesRouter.createFrame(request, response);
+    });
 
   });
 
   describe('#_getContractForShard', function() {
 
-    it.skip('should callback with error if no offer received');
+    it('should callback with error if no offer received', function(done) {
+      var _getStorageOffer = sinon.stub(
+        framesRouter.network,
+        'getStorageOffer'
+      ).callsArgWith(2, new Error('No storage offers received'));
+      var _contractsLoad = sinon.stub(
+        framesRouter.contracts,
+        'load'
+      ).callsArgWith(1, new Error('Contract not found'));
+      var contract = new storj.Contract({
+        data_hash: storj.utils.rmd160('data')
+      });
+      var audits = new storj.AuditStream(3);
+      framesRouter._getContractForShard(contract, audits, [], function(err) {
+        _contractsLoad.restore();
+        _getStorageOffer.restore();
+        expect(err.message).to.equal('No storage offers received');
+        done();
+      });
+    });
 
-    it.skip('should callback with error if contract cannot save');
+    it('should callback with error if contract cannot save', function(done) {
+      var contract = new storj.Contract({
+        data_hash: storj.utils.rmd160('data')
+      });
+      var audits = new storj.AuditStream(3);
+      audits.getPublicRecord = sinon.stub().returns([]);
+      audits.getPrivateRecord = sinon.stub().returns({});
+      var _contractsLoad = sinon.stub(
+        framesRouter.contracts,
+        'load'
+      ).callsArgWith(1, new Error('Contract not found'));
+      var _getStorageOffer = sinon.stub(
+        framesRouter.network,
+        'getStorageOffer'
+      ).callsArgWith(2, null, storj.Contact({
+        address: '127.0.0.1',
+        port: 1337,
+        nodeID: storj.utils.rmd160('farmer')
+      }), contract);
+      var _contractsSave = sinon.stub(
+        framesRouter.contracts,
+        'save'
+      ).callsArgWith(1, new Error('Failed to save'));
+      framesRouter._getContractForShard(
+        contract,
+        audits,
+        [],
+        function(err) {
+          _contractsLoad.restore();
+          _getStorageOffer.restore();
+          _contractsSave.restore();
+          expect(err.message).to.equal('Failed to save');
+          done();
+        }
+      );
+    });
 
-    it.skip('should callback with farmer and contract');
+    it('should callback with farmer and contract', function(done) {
+      var contract = new storj.Contract({
+        data_hash: storj.utils.rmd160('data')
+      });
+      var audits = new storj.AuditStream(3);
+      audits.getPublicRecord = sinon.stub().returns([]);
+      audits.getPrivateRecord = sinon.stub().returns({});
+      var _contractsLoad = sinon.stub(
+        framesRouter.contracts,
+        'load'
+      ).callsArgWith(1, new Error('Contract not found'));
+      var _getStorageOffer = sinon.stub(
+        framesRouter.network,
+        'getStorageOffer'
+      ).callsArgWith(2, null, storj.Contact({
+        address: '127.0.0.1',
+        port: 1337,
+        nodeID: storj.utils.rmd160('farmer')
+      }), contract);
+      var _contractsSave = sinon.stub(
+        framesRouter.contracts,
+        'save'
+      ).callsArgWith(1, null);
+      framesRouter._getContractForShard(
+        contract,
+        audits,
+        [],
+        function(err, farmer, contract2) {
+          _contractsLoad.restore();
+          _getStorageOffer.restore();
+          _contractsSave.restore();
+          expect(farmer.nodeID).to.equal(storj.utils.rmd160('farmer'));
+          expect(contract2.get('data_hash')).to.equal(
+            contract.get('data_hash')
+          );
+          done();
+        }
+      );
+    });
 
   });
 
@@ -61,35 +193,320 @@ describe('FramesRouter', function() {
 
   describe('#destroyFrameById', function() {
 
-    it.skip('should return internal error if bucket entry query fails');
+    it('should return internal error if entry query fails', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'DELETE',
+        url: '/frames/frameid',
+        params: {
+          frame: 'frameid'
+        }
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _bucketEntryFindOne = sinon.stub(
+        framesRouter.storage.models.BucketEntry,
+        'findOne'
+      ).callsArgWith(1, new Error('Panic!'));
+      framesRouter.destroyFrameById(request, response, function(err) {
+        _bucketEntryFindOne.restore();
+        expect(err.message).to.equal('Panic!');
+        done();
+      });
+    });
 
-    it.skip('should return bad request if frame is ref\'d by entry');
+    it('should return bad request if frame is ref\'d by entry', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'DELETE',
+        url: '/frames/frameid',
+        params: {
+          frame: 'frameid'
+        }
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _bucketEntryFindOne = sinon.stub(
+        framesRouter.storage.models.BucketEntry,
+        'findOne'
+      ).callsArgWith(1, null, new framesRouter.storage.models.BucketEntry({
+        user: someUser._id
+      }));
+      framesRouter.destroyFrameById(request, response, function(err) {
+        _bucketEntryFindOne.restore();
+        expect(err.message).to.equal(
+          'Refusing to destroy frame that is referenced by a bucket entry'
+        );
+        done();
+      });
+    });
 
-    it.skip('should return internal error if frame query fails');
+    it('should return internal error if frame query fails', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'DELETE',
+        url: '/frames/frameid',
+        params: {
+          frame: 'frameid'
+        }
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _bucketEntryFindOne = sinon.stub(
+        framesRouter.storage.models.BucketEntry,
+        'findOne'
+      ).callsArgWith(1, null, null);
+      var _frameFindOne = sinon.stub(
+        framesRouter.storage.models.Frame,
+        'findOne'
+      ).callsArgWith(1, new Error('Panic!'));
+      framesRouter.destroyFrameById(request, response, function(err) {
+        _frameFindOne.restore();
+        _bucketEntryFindOne.restore();
+        expect(err.message).to.equal('Panic!');
+        done();
+      });
+    });
 
-    it.skip('should return not found if no frame found');
+    it('should return not found if no frame found', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'DELETE',
+        url: '/frames/frameid',
+        params: {
+          frame: 'frameid'
+        }
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _bucketEntryFindOne = sinon.stub(
+        framesRouter.storage.models.BucketEntry,
+        'findOne'
+      ).callsArgWith(1, null, null);
+      var _frameFindOne = sinon.stub(
+        framesRouter.storage.models.Frame,
+        'findOne'
+      ).callsArgWith(1, null, null);
+      framesRouter.destroyFrameById(request, response, function(err) {
+        _frameFindOne.restore();
+        _bucketEntryFindOne.restore();
+        expect(err.message).to.equal('Frame not found');
+        done();
+      });
+    });
 
-    it.skip('should return internal error if remove fails');
+    it('should return internal error if remove fails', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'DELETE',
+        url: '/frames/frameid',
+        params: {
+          frame: 'frameid'
+        }
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _frameRemove = sinon.stub(
+        framesRouter.storage.models.Frame.prototype,
+        'remove'
+      ).callsArgWith(0, new Error('Panic!'));
+      var _bucketEntryFindOne = sinon.stub(
+        framesRouter.storage.models.BucketEntry,
+        'findOne'
+      ).callsArgWith(1, null, null);
+      var _frameFindOne = sinon.stub(
+        framesRouter.storage.models.Frame,
+        'findOne'
+      ).callsArgWith(1, null, new framesRouter.storage.models.Frame({
+        user: someUser._id
+      }));
+      framesRouter.destroyFrameById(request, response, function(err) {
+        _frameFindOne.restore();
+        _bucketEntryFindOne.restore();
+        _frameRemove.restore();
+        expect(err.message).to.equal('Panic!');
+        done();
+      });
+    });
 
-    it.skip('should return a 204 if delete succeeds');
+    it('should return a 204 if delete succeeds', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'DELETE',
+        url: '/frames/frameid',
+        params: {
+          frame: 'frameid'
+        }
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _frameRemove = sinon.stub(
+        framesRouter.storage.models.Frame.prototype,
+        'remove'
+      ).callsArgWith(0, null);
+      var _bucketEntryFindOne = sinon.stub(
+        framesRouter.storage.models.BucketEntry,
+        'findOne'
+      ).callsArgWith(1, null, null);
+      var _frameFindOne = sinon.stub(
+        framesRouter.storage.models.Frame,
+        'findOne'
+      ).callsArgWith(1, null, new framesRouter.storage.models.Frame({
+        user: someUser._id
+      }));
+      response.on('end', function() {
+        _frameFindOne.restore();
+        _bucketEntryFindOne.restore();
+        _frameRemove.restore();
+        expect(response.statusCode).to.equal(204);
+        done();
+      });
+      framesRouter.destroyFrameById(request, response);
+    });
 
   });
 
   describe('#getFrames', function() {
 
-    it.skip('should return internal error if query fails');
+    it('should return internal error if query fails', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/frames'
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _frameFind = sinon.stub(
+        framesRouter.storage.models.Frame,
+        'find'
+      ).callsArgWith(1, new Error('Panic!'));
+      framesRouter.getFrames(request, response, function(err) {
+        _frameFind.restore();
+        expect(err.message).to.equal('Panic!');
+        done();
+      });
+    });
 
-    it.skip('should return frame list');
+    it('should return frame list', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/frames'
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var frame1 = new framesRouter.storage.models.Frame({
+        user: someUser._id
+      });
+      var frame2 = new framesRouter.storage.models.Frame({
+        user: someUser._id
+      });
+      var _frameFind = sinon.stub(
+        framesRouter.storage.models.Frame,
+        'find'
+      ).callsArgWith(1, null, [frame1, frame2]);
+      response.on('end', function() {
+        _frameFind.restore();
+        expect(response._getData()).to.have.lengthOf(2);
+        done();
+      });
+      framesRouter.getFrames(request, response);
+    });
 
   });
 
   describe('#getFrameById', function() {
 
-    it.skip('should return internal error if query fails');
+    it('should return internal error if query fails', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/frames/frameid',
+        params: {
+          frame: 'frameid'
+        }
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _frameFindOne = sinon.stub(
+        framesRouter.storage.models.Frame,
+        'findOne'
+      ).callsArgWith(1, new Error('Panic!'));
+      framesRouter.getFrameById(request, response, function(err) {
+        _frameFindOne.restore();
+        expect(err.message).to.equal('Panic!');
+        done();
+      });
+    });
 
-    it.skip('should return not found if no frame found');
+    it('should return not found if no frame found', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/frames/frameid',
+        params: {
+          frame: 'frameid'
+        }
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _frameFindOne = sinon.stub(
+        framesRouter.storage.models.Frame,
+        'findOne'
+      ).callsArgWith(1, null, null);
+      framesRouter.getFrameById(request, response, function(err) {
+        _frameFindOne.restore();
+        expect(err.message).to.equal('Frame not found');
+        done();
+      });
+    });
 
-    it.skip('should return the frame');
+    it('should return the frame', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/frames/frameid',
+        params: {
+          frame: 'frameid'
+        }
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _frameFindOne = sinon.stub(
+        framesRouter.storage.models.Frame,
+        'findOne'
+      ).callsArgWith(1, null, new framesRouter.storage.models.Frame({
+        user: someUser._id
+      }));
+      response.on('end', function() {
+        _frameFindOne.restore();
+        expect(response._getData().user).to.equal(someUser._id);
+        done();
+      });
+      framesRouter.getFrameById(request, response);
+    });
 
   });
 
