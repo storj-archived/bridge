5618fb3aa53ad2ebdfcf017b8a6893de433ccff5
Gordon Hall <gordon@gordonwritescode.com>
2016-11-02 13:04:31 -0400

    some test coverage for buckets

--- test/server/routes/buckets.unit.js
+++ test/server/routes/buckets.unit.js
@@ -1,63 +1,467 @@
-/* jshint unused:false */ // TODO: REMOVE ME
-
 'use strict';
 
+const httpMocks = require('node-mocks-http');
+const sinon = require('sinon');
+const storj = require('storj-lib');
 const expect = require('chai').expect;
+const EventEmitter = require('events').EventEmitter;
 const BucketsRouter = require('../../../lib/server/routes/buckets');
 
 describe('BucketsRouter', function() {
 
+  var bucketsRouter = new BucketsRouter(
+    require('../../_fixtures/router-opts')
+  );
+  var someUser = new bucketsRouter.storage.models.User({
+    _id: 'gordon@storj.io',
+    hashpass: storj.utils.sha256('password')
+  });
+
   describe('#getBuckets', function() {
 
-    it.skip('should internal error if query fails');
+    it('should internal error if query fails', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/buckets'
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _bucketFind = sinon.stub(
+        bucketsRouter.storage.models.Bucket,
+        'find'
+      ).callsArgWith(1, new Error('Panic!'));
+      bucketsRouter.getBuckets(request, response, function(err) {
+        _bucketFind.restore();
+        expect(err.message).to.equal('Panic!');
+        done();
+      });
+    });
 
-    it.skip('should return buckets');
+    it('should return buckets', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/buckets'
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _bucketFind = sinon.stub(
+        bucketsRouter.storage.models.Bucket,
+        'find'
+      ).callsArgWith(1, null, [
+        new bucketsRouter.storage.models.Bucket({
+          user: someUser._id
+        })
+      ]);
+      response.on('end', function() {
+        _bucketFind.restore();
+        expect(response._getData()).to.have.lengthOf(1);
+        done();
+      });
+      bucketsRouter.getBuckets(request, response);
+    });
 
   });
 
   describe('#getBucketById', function() {
 
-    it.skip('should internal error if query fails');
+    it('should internal error if query fails', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/buckets/:bucket_id',
+        params: {
+          id: 'bucketid'
+        }
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _bucketFindOne = sinon.stub(
+        bucketsRouter.storage.models.Bucket,
+        'findOne'
+      ).callsArgWith(1, new Error('Panic!'));
+      bucketsRouter.getBucketById(request, response, function(err) {
+        _bucketFindOne.restore();
+        expect(err.message).to.equal('Panic!');
+        done();
+      });
+    });
 
-    it.skip('should not found error if no bucket');
+    it('should not found error if no bucket', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/buckets/:bucket_id',
+        params: {
+          id: 'bucketid'
+        }
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _bucketFindOne = sinon.stub(
+        bucketsRouter.storage.models.Bucket,
+        'findOne'
+      ).callsArgWith(1, null, null);
+      bucketsRouter.getBucketById(request, response, function(err) {
+        _bucketFindOne.restore();
+        expect(err.message).to.equal('Bucket not found');
+        done();
+      });
+    });
 
-    it.skip('should return bucket if found');
+    it('should return bucket if found', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/buckets/:bucket_id',
+        params: {
+          id: 'bucketid'
+        }
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _bucketFindOne = sinon.stub(
+        bucketsRouter.storage.models.Bucket,
+        'findOne'
+      ).callsArgWith(1, null, new bucketsRouter.storage.models.Bucket({
+        user: someUser._id
+      }));
+      response.on('end', function() {
+        _bucketFindOne.restore();
+        expect(response._getData().user).to.equal('gordon@storj.io');
+        done();
+      });
+      bucketsRouter.getBucketById(request, response);
+    });
 
   });
 
   describe('#createBucket', function() {
 
-    it.skip('should bad request error if invalid pubkey given');
+    it('should bad request error if invalid pubkey given', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/buckets',
+        body: {
+          pubkeys: [
+            'i am an invalid public key'
+          ]
+        }
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      bucketsRouter.createBucket(request, response, function(err) {
+        expect(err.message).to.equal('Invalid public key supplied');
+        done();
+      });
+    });
 
-    it.skip('should internal error if creation fails');
+    it('should internal error if creation fails', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/buckets',
+        body: {}
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _bucketCreate = sinon.stub(
+        bucketsRouter.storage.models.Bucket,
+        'create'
+      ).callsArgWith(2, new Error('Failed to create bucket'));
+      bucketsRouter.createBucket(request, response, function(err) {
+        _bucketCreate.restore();
+        expect(err.message).to.equal('Failed to create bucket');
+        done();
+      });
+    });
 
-    it.skip('should return the created bucket');
+    it('should return the created bucket', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/buckets',
+        body: {}
+      });
+      request.user = someUser;
+      request.pubkey = { _id: storj.KeyPair().getPublicKey() };
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _bucketCreate = sinon.stub(
+        bucketsRouter.storage.models.Bucket,
+        'create'
+      ).callsArgWith(2, null, new bucketsRouter.storage.models.Bucket({
+        user: someUser._id,
+        pubkeys: [request.pubkey._id]
+      }));
+      response.on('end', function() {
+        _bucketCreate.restore();
+        expect(response._getData().pubkeys[0]).to.equal(request.pubkey._id);
+        expect(response._getData().user).to.equal('gordon@storj.io');
+        done();
+      });
+      bucketsRouter.createBucket(request, response);
+    });
 
   });
 
   describe('#destroyBucketById', function() {
 
-    it.skip('should internal error if query fails');
+    it('should internal error if query fails', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'DELETE',
+        url: '/buckets/:bucket_id'
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _bucketFindOne = sinon.stub(
+        bucketsRouter.storage.models.Bucket,
+        'findOne'
+      ).callsArgWith(1, new Error('Failed to lookup bucket'));
+      bucketsRouter.destroyBucketById(request, response, function(err) {
+        _bucketFindOne.restore();
+        expect(err.message).to.equal('Failed to lookup bucket');
+        done();
+      });
+    });
 
-    it.skip('should not found error if no bucket');
+    it('should not found error if no bucket', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'DELETE',
+        url: '/buckets/:bucket_id'
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _bucketFindOne = sinon.stub(
+        bucketsRouter.storage.models.Bucket,
+        'findOne'
+      ).callsArgWith(1, null, null);
+      bucketsRouter.destroyBucketById(request, response, function(err) {
+        _bucketFindOne.restore();
+        expect(err.message).to.equal('Bucket not found');
+        done();
+      });
+    });
 
-    it.skip('should internal error if deletion fails');
+    it('should internal error if deletion fails', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'DELETE',
+        url: '/buckets/:bucket_id'
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var bucket = new bucketsRouter.storage.models.Bucket({
+        user: someUser._id
+      });
+      var _bucketFindOne = sinon.stub(
+        bucketsRouter.storage.models.Bucket,
+        'findOne'
+      ).callsArgWith(1, null, bucket);
+      var _bucketRemove = sinon.stub(bucket, 'remove').callsArgWith(
+        0,
+        new Error('Failed to remove bucket')
+      );
+      bucketsRouter.destroyBucketById(request, response, function(err) {
+        _bucketFindOne.restore();
+        _bucketRemove.restore();
+        expect(err.message).to.equal('Failed to remove bucket');
+        done();
+      });
+    });
 
-    it.skip('should return 204 on success');
+    it('should return 204 on success', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'DELETE',
+        url: '/buckets/:bucket_id'
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var bucket = new bucketsRouter.storage.models.Bucket({
+        user: someUser._id
+      });
+      var _bucketFindOne = sinon.stub(
+        bucketsRouter.storage.models.Bucket,
+        'findOne'
+      ).callsArgWith(1, null, bucket);
+      var _bucketRemove = sinon.stub(bucket, 'remove').callsArg(0);
+      response.on('end', function() {
+        _bucketFindOne.restore();
+        _bucketRemove.restore();
+        expect(response.statusCode).to.equal(204);
+        done();
+      });
+      bucketsRouter.destroyBucketById(request, response);
+    });
 
   });
 
   describe('#updateBucketById', function() {
 
-    it.skip('should internal error if query fails');
+    it('should internal error if query fails', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'PATCH',
+        url: '/buckets/:bucket_id',
+        body: {
+          name: 'Some Bucket'
+        }
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _bucketFindOne = sinon.stub(
+        bucketsRouter.storage.models.Bucket,
+        'findOne'
+      ).callsArgWith(1, new Error('Failed to lookup bucket'));
+      bucketsRouter.updateBucketById(request, response, function(err) {
+        _bucketFindOne.restore();
+        expect(err.message).to.equal('Failed to lookup bucket');
+        done();
+      });
+    });
 
-    it.skip('should not found error if bucket not found');
+    it('should not found error if bucket not found', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'PATCH',
+        url: '/buckets/:bucket_id',
+        body: {
+          name: 'Some Bucket'
+        }
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _bucketFindOne = sinon.stub(
+        bucketsRouter.storage.models.Bucket,
+        'findOne'
+      ).callsArgWith(1, null, null);
+      bucketsRouter.updateBucketById(request, response, function(err) {
+        _bucketFindOne.restore();
+        expect(err.message).to.equal('Bucket not found');
+        done();
+      });
+    });
 
-    it.skip('should bad request error if invalid pubkey');
+    it('should bad request error if invalid pubkey', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'PATCH',
+        url: '/buckets/:bucket_id',
+        body: {
+          name: 'Some Bucket',
+          pubkeys: ['some invalid public key']
+        }
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var _bucketFindOne = sinon.stub(
+        bucketsRouter.storage.models.Bucket,
+        'findOne'
+      ).callsArgWith(1, null, new bucketsRouter.storage.models.Bucket({
+        user: someUser._id
+      }));
+      bucketsRouter.updateBucketById(request, response, function(err) {
+        _bucketFindOne.restore();
+        expect(err.message).to.equal('Invalid public key supplied');
+        done();
+      });
+    });
 
-    it.skip('should internal error if save fails');
+    it('should internal error if save fails', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'PATCH',
+        url: '/buckets/:bucket_id',
+        body: {
+          name: 'Some Bucket'
+        }
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var bucket = new bucketsRouter.storage.models.Bucket({
+        user: someUser._id
+      });
+      var _bucketFindOne = sinon.stub(
+        bucketsRouter.storage.models.Bucket,
+        'findOne'
+      ).callsArgWith(1, null, bucket);
+      var _bucketSave = sinon.stub(bucket, 'save').callsArgWith(
+        0,
+        new Error('Failed to save bucket')
+      );
+      bucketsRouter.updateBucketById(request, response, function(err) {
+        _bucketFindOne.restore();
+        _bucketSave.restore();
+        expect(err.message).to.equal('Failed to save bucket');
+        done();
+      });
+    });
 
-    it.skip('should return bucket if success');
+    it('should return bucket if success', function(done) {
+      var request = httpMocks.createRequest({
+        method: 'PATCH',
+        url: '/buckets/:bucket_id',
+        body: {
+          name: 'Some Bucket'
+        }
+      });
+      request.user = someUser;
+      var response = httpMocks.createResponse({
+        req: request,
+        eventEmitter: EventEmitter
+      });
+      var bucket = new bucketsRouter.storage.models.Bucket({
+        user: someUser._id
+      });
+      var _bucketFindOne = sinon.stub(
+        bucketsRouter.storage.models.Bucket,
+        'findOne'
+      ).callsArgWith(1, null, bucket);
+      var _bucketSave = sinon.stub(bucket, 'save').callsArgWith(0);
+      response.on('end', function() {
+        _bucketFindOne.restore();
+        _bucketSave.restore();
+        expect(response._getData().user).to.equal('gordon@storj.io');
+        done();
+      });
+      bucketsRouter.updateBucketById(request, response);
+    });
 
   });
 
