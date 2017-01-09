a9e10209eddc194b4b4ccc37bd610d69761b03ec
Braydon Fuller <braydon@storj.io>
2017-01-09 17:44:31 -0500

    Update config in monitor bin

--- bin/storj-monitor.js
+++ bin/storj-monitor.js
@@ -3,14 +3,14 @@
 'use strict';
 
 const program = require('commander');
-const Config = require('../lib/config');
+const Config = require('../lib/monitor/config');
 const Monitor = require('../lib/monitor');
 
 program.version(require('../package').version);
 program.option('-c, --config <path_to_config_file>', 'path to the config file');
 program.parse(process.argv);
 
-var config = new Config(process.env.NODE_ENV || 'develop', program.config, program.datadir);
+var config = new Config(program.config);
 var monitor = new Monitor(config);
 
 monitor.start(function(err) {
