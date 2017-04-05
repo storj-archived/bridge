02a2e1ba114e84df756d453d49992149f6a4d1a9
James Prestwich <James@storj.io>
2017-04-04 18:23:56 -0600

    add traits if missing, add tests for email insertion, fix bugs

--- test/vendor/analytics.unit.js
+++ test/vendor/analytics.unit.js
@@ -41,5 +41,32 @@ describe('BridgeAnalytics', function() {
       sinon.assert.calledOnce(Analytics.prototype.identify);
       done();
     });
+
+    it('should add message.traits if there is none', function (done) {
+      bridgeAnalytics.identify(0, {});
+      sinon.assert.calledWith(
+        Analytics.prototype.identify,
+        {
+          traits: {
+            email: 'undefined@user.storj.io'
+          }
+        },
+        undefined);
+      done();
+    });
+
+    it('should add an email to message.traits', function (done) {
+      bridgeAnalytics.identify(0, {userId: 'a', traits: {}});
+      sinon.assert.calledWith(
+        Analytics.prototype.identify,
+        {
+          userId: 'a',
+          traits: {
+            email: 'a@user.storj.io'
+          }
+        },
+        undefined);
+      done();
+    });
   });
 });
