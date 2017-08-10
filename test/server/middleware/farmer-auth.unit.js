44389f937785e60b8dee38d8dbe50541b5559839
Braydon Fuller <braydon@storj.io>
2017-08-10 16:00:28 -0400

    Test farmer signature

--- test/server/middleware/farmer-auth.unit.js
+++ test/server/middleware/farmer-auth.unit.js
@@ -3,6 +3,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 const sinon = require('sinon');
+const secp256k1 = require('secp256k1');
 const auth = require('../../../lib/server/middleware/farmer-auth');
 
 describe('Farmer Authentication Middleware', function() {
@@ -11,19 +12,14 @@ describe('Farmer Authentication Middleware', function() {
 
   describe('#authFarmer', function() {
     it('will give error for invalid timestamp', function() {
-
     });
     it('will give error for invalid pubkey', function() {
-
     });
     it('will give error for invalid nodeid', function() {
-
     });
     it('will give error if missing body', function() {
-
     });
     it('will give error for invalid signature', function() {
-
     });
     it('will continue without error', function() {
     });
@@ -108,26 +104,100 @@ describe('Farmer Authentication Middleware', function() {
 
   describe('#checkSig', function() {
     it('will verify that signature is correct', function() {
-
+      let privkey = '8e812246e61ea983efdd4d1c86e246832667a4e4b8fc2d9ff01c534c8a6d7681';
+      let pubkey = '03ea58aff546b28bb748d560ad05bb78c0e1b9f5de8edc5021494833c73c224284';
+      let sig = null;
+      let req = {
+        headers: function(key) {
+          if (key === 'x-node-timestamp') {
+            return '1502390208007';
+          } else if (key === 'x-node-pubkey') {
+            return pubkey;
+          } else if (key === 'x-node-signature') {
+            return sig;
+          }
+        },
+        method: 'POST',
+        protocol: 'https',
+        originalUrl: '/contacts?someQueryArgument=value',
+        get: function(key) {
+          if (key === 'host') {
+            return 'api.storj.io';
+          }
+        },
+        rawbody: Buffer.from('{"key": "value"}', 'utf8')
+      }
+      const sighash = auth.getSigHash(req);
+      const sigObj = secp256k1.sign(sighash, Buffer.from(privkey, 'hex'));
+      sig = secp256k1.signatureExport(sigObj.signature).toString('hex');
+      expect(auth.checkSig(req)).to.equal(true);
     });
     it('will verify that signature is incorrect', function() {
-
+      let privkey = '8e812246e61ea983efdd4d1c86e246832667a4e4b8fc2d9ff01c534c8a6d7681';
+      let pubkey = '03ea58aff546b28bb748d560ad05bb78c0e1b9f5de8edc5021494833c73c224284';
+      let timestamp = '1502390208007';
+      let sig = null;
+      let req = {
+        headers: function(key) {
+          if (key === 'x-node-timestamp') {
+            return timestamp;
+          } else if (key === 'x-node-pubkey') {
+            return pubkey;
+          } else if (key === 'x-node-signature') {
+            return sig;
+          }
+        },
+        method: 'POST',
+        protocol: 'https',
+        originalUrl: '/contacts?someQueryArgument=value',
+        get: function(key) {
+          if (key === 'host') {
+            return 'api.storj.io';
+          }
+        },
+        rawbody: Buffer.from('{"key": "value"}', 'utf8')
+      }
+      const sighash = auth.getSigHash(req);
+      const sigObj = secp256k1.sign(sighash, Buffer.from(privkey, 'hex'));
+      sig = secp256k1.signatureExport(sigObj.signature).toString('hex');
+      // change the data so the signature fails
+      timestamp = '1502390208009';
+      expect(auth.checkSig(req)).to.equal(false);
     });
   });
 
   describe('#isHexaString', function() {
     it('return false for nonhex string', function() {
+      expect(auth.isHexString('zz')).to.equal(false);
+    });
+    it('return false for nonhex string (incorrect bytes)', function() {
+      expect(auth.isHexString('aaa')).to.equal(false);
     });
     it('return true for hex string', function() {
+      expect(auth.isHexString('038c')).to.equal(true);
     });
   });
 
   describe('#getSigHash', function() {
     it('will get the expected hash from the request', function() {
-
-    });
-    it('will get the expected hash from the request (utf-8)', function() {
-
+      let req = {
+        headers: function(key) {
+          if (key === 'x-node-timestamp') {
+            return '1502390208007';
+          }
+        },
+        method: 'POST',
+        protocol: 'https',
+        originalUrl: '/contacts?someQueryArgument=value',
+        get: function(key) {
+          if (key === 'host') {
+            return 'api.storj.io';
+          }
+        },
+        rawbody: Buffer.from('{"key": "value"}', 'utf8')
+      }
+      const hash = auth.getSigHash(req);
+      expect(hash.toString('hex')).to.equal('59146f00725c9c052ef5ec6acd63f3842728c9d191ac146668204de6ed4a648b');
     });
   });
 });
